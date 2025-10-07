995f5b2fe970f00d4d184e47e143e8f9a71dd752
Anurag <anurag789p@gmail.com>
2025-10-07 09:38:58 +0530

    feat: implement sign-up form with validation and input components

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,75 @@
-import React from 'react'
+"use client"
+
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form"
 
 const SignUp = () => {
-  return (
-    <div>
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+      defaultValues: {
+      fullName: '',
+      email: '',
+      password: '',
+      country: 'India',
+      investmentGoals: 'Growth',
+      riskTolerance: 'Medium',
+      preferredIndustry: 'Technology',
+      },
+      mode: 'onBlur'
+  },);
+
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log(data);
+    } catch (e) {
+      console.error(e);
       
-    </div>
+    }
+  }
+
+  return (
+    <>
+      <h1 className="form-title">Sign Up & Personalize</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+            name="fullName"
+            label="Full Name"
+            placeholder="John Doe"
+            register={register}
+            error={errors.fullName}
+            validation={{ required: "Full Name is required", minlength: 2 }}
+        />
+
+         <InputField
+            name="email"
+            label="Email"
+            placeholder="contact@progamii.com"
+            register={register}
+            error={errors.email}
+            validation={{ required: "Email is required", pattern: /^\w+@\w+\.\w+$/, message: "Invalid email address" }}
+        />
+
+         <InputField
+            name="password"
+            label="Password"
+            placeholder="Enter a strong password"
+            type="password"
+            register={register}
+            error={errors.password}
+            validation={{ required: "Password is required", minlength: 8 }}
+        />
+
+        <Button type="submit" className="yellow-btn w-full mt-5" disabled={isSubmitting}>
+          {isSubmitting ? "Creating Account..." : "Start Your Investing Journey"}
+        </Button>
+      </form>
+    </>
   )
 }
 
